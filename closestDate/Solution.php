b81df1404491fde3319cb5ab292dd5ac09854f5c
Tiago <tiagooom88@gmail.com>
2024-08-06 00:03:29 -0200

    terminado

--- closestDate/Solution.php
+++ closestDate/Solution.php
@@ -4,14 +4,33 @@ class Solution
 {
     public function closestDate($arr, $q, $queries)
     {
-
-        $data1 = '10/1/2000';
-        $data2 = '10/1/2000';
-
-        $data = DateTime::createFromFormat('d/m/Y', $data1);
-
-        $teste = $data;
-
-        return $teste;
+        
+        foreach ($queries as $dateq)
+        {
+            $closest = -1;
+            $formateddateq = DateTime::createFromFormat('d/m/Y', $dateq);
+            foreach ($arr as $datea)
+            {
+                $formateddatea = DateTime::createFromFormat('d/m/Y', $datea);
+                if ($formateddatea > $formateddateq) 
+                {
+                    $intervalo = $formateddateq->diff($formateddatea)->days;
+                    if ($closest == -1) 
+                    {
+                        $closest = $intervalo;
+                        $closestDate = $formateddatea->format('d/m/Y');
+                    
+                    }
+                    elseif ($intervalo < $closest)
+                    {
+                        $closest = $intervalo;
+                        $closestDate =$formateddatea->format('d/m/Y');
+                    } 
+                }
+               //echo $dateq.' '.$datea.' = '.$intervalo.'<br>'; compara datas
+            }
+            $closestDates[] = ($closest != -1) ? $closestDate : -1;
+        }
+        return $closestDates;
     }
 }
--- closestDate/index.php
+++ closestDate/index.php
@@ -3,9 +3,9 @@
 //Problem: Queries to find the future closest date
 require_once('Solution.php');
 
-$entrada = ["22/4/1233", "1/3/633", "23/5/56645", "4/12/233"];
 $q = 2;
 $query = ["23/3/4345", "12/3/2"];
+$entrada = ["22/4/1233", "1/3/633", "23/5/4345", "4/12/233"];
 
 $solution = new Solution();
 
