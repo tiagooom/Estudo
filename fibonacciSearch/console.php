6115eeb0aeb8bacc4bdeea78a891742891e9a86f
Tiago <tiagooom88@gmail.com>
2024-08-23 16:50:07 -0200

    feat: gerando a sequencia fibonacci para a busca

--- fibonacciSearch/console.php
+++ fibonacciSearch/console.php
@@ -5,7 +5,18 @@ require __DIR__ . '/vendor/autoload.php';
 use App\Command\FibonacciSearchCommand;
 use Symfony\Component\Console\Application;
 use App\Command\GreetCommand;
+use App\FibonacciSearcher;
 
+    $array = [10, 20, 30, 40, 50, 60, 70];
+    $find = 10;
+
+    $fibonacci = new FibonacciSearcher();
+
+    $teste = $fibonacci->fibSearch($array, $find);
+    echo '<pre>';
+    var_dump($teste);
+    echo '<pre>';
+    
 $application = new Application();
 
 // Registre o comando
@@ -13,3 +24,4 @@ $application->add(new GreetCommand());
 $application->add(new FibonacciSearchCommand());
 
 $application->run();
+var_dump('teste');
--- fibonacciSearch/src/app/FibonacciSearcher.php
+++ fibonacciSearch/src/app/FibonacciSearcher.php
@@ -10,14 +10,16 @@ class FibonacciSearcher
         $fibm1 = 1;
         $fibm = $fibm1 + $fibm2;
         $n = count($arr);
+        $fib = [$fibm2, $fibm1, $fibm];
 
         for ($i = 2; $i < $n; $i++) 
         {
-            $fibm += $fibm1;
-            $fibm1 = $fibm;
             $fibm2 = $fibm1;
+            $fibm1 = $fibm;
+            $fibm = $fibm1 + $fibm2;
+            $fib[] = $fibm;
         }
 
-        return $fibm2;
+        return $fib;
     }    
 }
