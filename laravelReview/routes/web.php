ecf3d90cbe9df30ddde51b5370ba61ec81ba5c37
Tiago <tiagooom88@gmail.com>
2024-08-30 10:01:27 -0200

    feat: adicionando tela incial com paginas home about e contact

--- laravelReview/routes/web.php
+++ laravelReview/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
