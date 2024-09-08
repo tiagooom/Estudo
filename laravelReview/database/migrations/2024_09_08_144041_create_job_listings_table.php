73c52d9616099354eed88a54ea80a5bf38ab99d6
Tiago <tiagooom88@gmail.com>
2024-09-08 13:02:05 -0200

    criando a tabela job com migration

--- laravelReview/database/migrations/2024_09_08_144041_create_job_listings_table.php
+++ laravelReview/database/migrations/2024_09_08_144041_create_job_listings_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('salary');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('job_listings');
     }
 };
