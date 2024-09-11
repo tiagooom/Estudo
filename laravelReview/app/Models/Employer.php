82ea3867b91a127001563fba740a12955714887e
Tiago <tiagooom88@gmail.com>
2024-09-10 23:35:15 -0200

    feat: adicionando relacionamentos entre os models de job e employer

--- laravelReview/app/Models/Employer.php
+++ laravelReview/app/Models/Employer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Employer extends Model
 {
     use HasFactory;
+
+    public function Job()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
