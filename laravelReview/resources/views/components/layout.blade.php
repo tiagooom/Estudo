fb81d271f4cd5e985a98c7733c4e862298cb97a1
Tiago <tiagooom88@gmail.com>
2024-09-17 16:48:09 -0200

    fix: corrigindo fechamento da tag head

--- laravelReview/resources/views/components/layout.blade.php
+++ laravelReview/resources/views/components/layout.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en"j class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-    <script src="https://cdn.tailwindcss.com/"></script>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Home</title>
+        <script src="https://cdn.tailwindcss.com/"></script>
+    </head>
     <body class="h-full">
     <!--
     This example requires updating your template:
@@ -111,7 +112,5 @@
         </div>
         </main>
     </div>
-  
     </body>
-</head>
 </html>
