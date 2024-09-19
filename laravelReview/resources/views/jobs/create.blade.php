452bc9a9a82bc47c4bc4b2dcbf34d608bc3110a1
Tiago <tiagooom88@gmail.com>
2024-09-19 20:05:26 -0200

    feat: Botao criar e validando inputs de entrada da criacao de job

--- laravelReview/resources/views/jobs/create.blade.php
+++ laravelReview/resources/views/jobs/create.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
-    <x-slot:heading>
-        Job Create
-    </x-slot:heading>
-    <!--
+  <x-slot:heading>
+    Job Create
+  </x-slot:heading>
+  <!--
   This example requires some changes to your config:
   
   ```
@@ -16,42 +16,65 @@
   }
   ```
 -->
-<form method="POST" action="/jobs">
+  <form method="POST" action="/jobs">
     @csrf
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
         <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful details from you.</p>
-  
+
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
-              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Holder">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input type="text" name="title" id="title"
+                  class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Shift Holder" required>
               </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-            <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
-            <div class="mt-2">
-              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50.000 per year.">
-              </div>
+              @error('title')
+                <p class="mt-1 text-red-500 italic"> {{ $message }} </p>
+            @enderror
             </div>
           </div>
         </div>
 
+        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="sm:col-span-4">
+            <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input type="text" name="salary" id="salary"
+                  class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="$50.000 per year." required>
+              </div>
+            </div>
+            @error('salary')
+                <p class="mt-1 text-red-500 italic"> {{ $message }} </p>
+            @enderror
+          </div>
+        </div>
+        {{--
+        @if ($errors->any())
+        <div class="mt-10">
+          <ul>
+            @foreach ($errors->all() as $error)
+            <li class='text-sm text-red-500 italic'> {{ $error }} </li>
+            @endforeach
+          </ul>
+        </div>
+        @endif
       </div>
+      --}}
     </div>
-  
+
     <div class="mt-6 flex items-center justify-end gap-x-6">
       <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      <button type="submit"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
     </div>
   </form>
-  
+
 </x-layout>
