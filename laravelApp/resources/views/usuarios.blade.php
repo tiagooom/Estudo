937dc8e8ea433adea3520fb4b6d4854a0acd45f0
Tiago <tiagooom88@gmail.com>
2024-09-25 19:59:03 -0200

    feat: mostrando usuarios com paginacao traduzida

--- laravelApp/resources/views/usuarios.blade.php
+++ laravelApp/resources/views/usuarios.blade.php
@@ -1,3 +1,10 @@
 <x-layout title='Usuários'>
-    Usuários
+    <div>
+        @foreach ($usuarios as $usuario)
+           <a href="/usuarios/{{ $usuario->id }}" class="block mt-4 py-4 border border-gray-200 rounded-lg"> <strong>{{ $usuario->nome }}</strong> </a>
+        @endforeach
+    </div>
+    <div class="mt-4">
+        {{ $usuarios->links() }}
+    </div>
 </x-layout>
--- laravelApp/routes/web.php
+++ laravelApp/routes/web.php
@@ -19,6 +19,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/welcome', 'welcome');
 Route::view('/', 'home');
-Route::view('/usuarios', 'usuarios');
+
+//Route::view('/usuarios', 'usuarios');
+Route::get('/usuarios', function() {
+    $usuarios = App\Models\Usuario::cursorPaginate(6);
+
+    return (view('usuarios', ['usuarios' => $usuarios]));
+});
+
 Route::view('/tarefas', 'tarefas');
 
