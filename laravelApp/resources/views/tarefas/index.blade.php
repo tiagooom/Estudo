2f97b69a5576eaf4d9a3afbe1ed3c003c98f63a6
Tiago <tiagooom88@gmail.com>
2024-10-03 20:16:06 -0200

    style: mensagem de criacao mais legivel

--- laravelApp/resources/views/tarefas/index.blade.php
+++ laravelApp/resources/views/tarefas/index.blade.php
@@ -6,7 +6,7 @@
         </div>
     </x-slot:heading>
     @if (session('success'))
-        <div class="bg-green-100 border border-green-400 text-red-500 px-4 py-3 rounded relative" role="alert">
+        <div class="bg-green-100 border border-green-400 text-green-800 px-4 py-3 rounded relative" role="alert">
             <span class="block sm:inline">{{ session('success') }}</span>
         </div>
     @endif
