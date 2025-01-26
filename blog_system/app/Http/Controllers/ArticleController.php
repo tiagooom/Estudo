24fdd8d71a548be53ed913218d98d4acc57b4061
Tiago <tiagooom88@gmail.com>
2025-01-26 20:28:32 -0300

    feat: metedo index para exibir formulario de artigos no controller resourse

--- blog_system/app/Http/Controllers/ArticleController.php
+++ blog_system/app/Http/Controllers/ArticleController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::all(); // Pega todos os artigos
+        return view('articles.index', compact('articles')); // Retorna para a view com os artigos
+    }
+
 }
