4db1bba590bec105d08e289e57f38e4d1d8c77d1
Tiago <tiagooom88@gmail.com>
2025-02-05 20:08:14 -0300

    feat: implementando os models do projeto

--- ecommerce/ecommerce-backend/app/Models/Order.php
+++ ecommerce/ecommerce-backend/app/Models/Order.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['user_id', 'total_price', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
+
--- ecommerce/ecommerce-backend/app/Models/Product.php
+++ ecommerce/ecommerce-backend/app/Models/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'price', 'quantity', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
+
