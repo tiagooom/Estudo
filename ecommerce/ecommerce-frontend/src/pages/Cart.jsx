b8e7ae5b46c2d23a4427ff76510093bcdba06459
Tiago <tiagooom88@gmail.com>
2025-02-05 10:03:39 -0300

    feat: adicionando rotas e componetes inicias no frontend

---
+++ ecommerce/ecommerce-frontend/src/pages/Cart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cart = () => {
+    return <h1>Meu Carrinho</h1>;
+};
+
+export default Cart;
---
+++ ecommerce/ecommerce-frontend/src/pages/ProductDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductDetail = () => {
+    return <h1>Detalhes do Produto</h1>;
+};
+
+export default ProductDetail;
