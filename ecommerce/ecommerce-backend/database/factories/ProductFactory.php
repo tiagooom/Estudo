c7936222467b9861c6eb31e24def93aa2fe72d51
Tiago <tiagooom88@gmail.com>
2025-02-05 21:36:16 -0300

    feat: implementando factories para produto e categorias

---
+++ ecommerce/ecommerce-backend/database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    // database/factories/ProductFactory.php
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 10, 100),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'category_id' => Category::factory(),
+        ];
+    }
+
+}
