257268f77934d9877ac835d23dc246cb11ab881f
Tiago <tiagooom88@gmail.com>
2025-02-10 18:08:33 -0300

    feat adicionando pagina de login e registro do material UI com suas devidas rotas

--- ecommerce/ecommerce-frontend/src/App.jsx
+++ ecommerce/ecommerce-frontend/src/App.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import AddProduct from './pages/AddProduct';
-import Cart from './pages/Cart';
-
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import AddProduct from "./pages/AddProduct";
+import Cart from "./pages/Cart";
 
 // Criação do tema com Material UI
 const theme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "light",
   },
 });
 
@@ -24,18 +23,29 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/products/add" element={<AddProduct />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Layout />
       </Router>
     </ThemeProvider>
+  );
+}
+
+function Layout() {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/login", "/register"];
+
+  return (
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/products/add" element={<AddProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
