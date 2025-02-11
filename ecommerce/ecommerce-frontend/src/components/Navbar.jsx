972bb9f82c0ecf30562b9498e404b74b16c34648
Tiago <tiagooom88@gmail.com>
2025-02-11 16:40:21 -0300

    feat: adicionando botao de login e registro ao navbar

--- ecommerce/ecommerce-frontend/src/components/Navbar.jsx
+++ ecommerce/ecommerce-frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
@@ -10,15 +10,26 @@ function Navbar() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Ecommerce
         </Typography>
-        {/* Botões de navegação */}
-        <Button color="inherit" component={Link} to="/">
-          Home
+
+        {/* Botões de navegação centralizados */}
+        <Box sx={{ flexGrow: 1}}>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/products">
+            Produtos
+          </Button>
+          <Button color="inherit" component={Link} to="/cart">
+            Carrinho
+          </Button>
+        </Box>
+
+        {/* Botões de login e registro à direita */}
+        <Button color="inherit" component={Link} to="/login">
+          Login
         </Button>
-        <Button color="inherit" component={Link} to="/products">
-          Produtos
-        </Button>
-        <Button color="inherit" component={Link} to="/cart">
-          Carrinho
+        <Button color="inherit" component={Link} to="/register">
+          Registrar
         </Button>
       </Toolbar>
     </AppBar>
@@ -26,4 +37,3 @@ function Navbar() {
 }
 
 export default Navbar
-
