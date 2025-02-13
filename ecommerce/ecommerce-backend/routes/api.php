2178567f87de42e3668ec44b425dcde913978b4e
Tiago <tiagooom88@gmail.com>
2025-02-13 10:57:27 -0300

    feat: adicionando rota de logout na api

--- ecommerce/ecommerce-backend/routes/api.php
+++ ecommerce/ecommerce-backend/routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 Route::apiResource('products', ProductController::class);
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json(['user' => $request->user()]);
