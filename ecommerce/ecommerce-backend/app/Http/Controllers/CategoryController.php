319fa18363cacfe6cdd36b309178331a78f9c6f5
Tiago <tiagooom88@gmail.com>
2025-02-15 16:35:01 -0300

    feat: implementacao de rota e controller para categorias

--- ecommerce/ecommerce-backend/app/Http/Controllers/CategoryController.php
+++ ecommerce/ecommerce-backend/app/Http/Controllers/CategoryController.php
@@ -2,47 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return response()->json($categories);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|unique:categories,name'
+        ]);
+
+        $category = Category::create($request->only('name'));
+
+        return response()->json($category, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return response()->json($category);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        $request->validate([
+            'name' => 'required|unique:categories,name,' . $category->id,
+        ]);
+
+        $category->update($request->only('name', 'description'));
+
+        return response()->json($category);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return response()->json(null, 204);
     }
 }
--- ecommerce/ecommerce-backend/routes/api.php
+++ ecommerce/ecommerce-backend/routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
