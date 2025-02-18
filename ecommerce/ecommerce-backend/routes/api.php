3edd32cf944ffcb9f9d65677e516fdd921a9acd8
Tiago <tiagooom88@gmail.com>
2025-02-18 12:19:38 -0300

    feat: adicionando e refatorando rotas de categoria e produto pra role admin e agrupando as mesmas

--- ecommerce/ecommerce-backend/routes/api.php
+++ ecommerce/ecommerce-backend/routes/api.php
@@ -6,14 +6,20 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::apiResource('products', ProductController::class);
-Route::apiResource('categories', CategoryController::class);
-
+// Rotas públicas
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::apiResource('products', ProductController::class)->only(['index', 'show']);;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return response()->json(['user' => $request->user()]);
+// Grupo de rotas que exigem autenticação via Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return response()->json(['user' => $request->user()]);
+    });
+
+    Route::middleware('admin')->group(function () {
+        Route::apiResource('products', ProductController::class)->except(['index', 'show']);;
+        Route::apiResource('categories', CategoryController::class);
+    });
 });
-
-
