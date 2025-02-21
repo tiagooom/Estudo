15a02785dba5454ddb4b0495f660c1a21b708272
Tiago <tiagooom88@gmail.com>
2025-02-21 10:05:59 -0300

    feat: implementando registro no controller do usuario

--- ecommerce/ecommerce-backend/app/Http/Controllers/AuthController.php
+++ ecommerce/ecommerce-backend/app/Http/Controllers/AuthController.php
@@ -4,9 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password'])
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
