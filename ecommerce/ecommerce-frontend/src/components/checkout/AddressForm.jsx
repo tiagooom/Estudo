8411c692cd7ab6db7caf866ae54967a15342da4a
Tiago <tiagooom88@gmail.com>
2025-03-01 18:42:30 -0300

    feat: traducoes e pegando dado da sessao para completar os campos

--- ecommerce/ecommerce-frontend/src/components/checkout/AddressForm.jsx
+++ ecommerce/ecommerce-frontend/src/components/checkout/AddressForm.jsx
@@ -1,125 +1,126 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
 import Grid from '@mui/material/Grid2';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 
 const FormGrid = styled(Grid)(() => ({
   display: 'flex',
   flexDirection: 'column',
 }));
+const address = JSON.parse(sessionStorage.getItem("addressData")) || {};
 
-export default function AddressForm() {
+
+console.log(address.firstName); // Agora você tem os dados de endereço disponíveis
+
+export default function AddressForm( {  } ) {
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [firstNameErrorMessage, setFirstNameErrorMessage] = useState(false);
+  
   return (
     <Grid container spacing={3}>
       <FormGrid size={{ xs: 12, md: 6 }}>
         <FormLabel htmlFor="first-name" required>
-          First name
+          Nome
         </FormLabel>
-        <OutlinedInput
+        <TextField
+          error={firstNameError}
+          helperText={firstNameErrorMessage}
           id="first-name"
           name="first-name"
-          type="name"
-          placeholder="John"
-          autoComplete="first name"
+          type="text"
+          placeholder="João"
+          autoComplete="given-name"
           required
           size="small"
+          defaultValue={address.firstName || ""}
+          variant="outlined"
+          color={firstNameError ? 'error' : 'primary'}
         />
       </FormGrid>
       <FormGrid size={{ xs: 12, md: 6 }}>
         <FormLabel htmlFor="last-name" required>
-          Last name
+          Sobrenome
         </FormLabel>
         <OutlinedInput
           id="last-name"
           name="last-name"
-          type="last-name"
-          placeholder="Snow"
-          autoComplete="last name"
+          type="text"
+          placeholder="Silva"
+          autoComplete="family-name"
           required
           size="small"
         />
       </FormGrid>
       <FormGrid size={{ xs: 12 }}>
-        <FormLabel htmlFor="address1" required>
-          Address line 1
+        <FormLabel htmlFor="address" required>
+          Endereço
         </FormLabel>
         <OutlinedInput
-          id="address1"
-          name="address1"
-          type="address1"
-          placeholder="Street name and number"
+          id="address"
+          name="address"
+          type="text"
+          placeholder="Nome da rua e número"
           autoComplete="shipping address-line1"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 12 }}>
-        <FormLabel htmlFor="address2">Address line 2</FormLabel>
-        <OutlinedInput
-          id="address2"
-          name="address2"
-          type="address2"
-          placeholder="Apartment, suite, unit, etc. (optional)"
-          autoComplete="shipping address-line2"
           required
           size="small"
         />
       </FormGrid>
       <FormGrid size={{ xs: 6 }}>
         <FormLabel htmlFor="city" required>
-          City
+          Cidade
         </FormLabel>
         <OutlinedInput
           id="city"
           name="city"
-          type="city"
-          placeholder="New York"
-          autoComplete="City"
+          type="text"
+          placeholder="São Paulo"
+          autoComplete="address-level2"
           required
           size="small"
         />
       </FormGrid>
       <FormGrid size={{ xs: 6 }}>
         <FormLabel htmlFor="state" required>
-          State
+          Estado
         </FormLabel>
         <OutlinedInput
           id="state"
           name="state"
-          type="state"
-          placeholder="NY"
-          autoComplete="State"
+          type="text"
+          placeholder="SP"
+          autoComplete="address-level1"
           required
           size="small"
         />
       </FormGrid>
       <FormGrid size={{ xs: 6 }}>
         <FormLabel htmlFor="zip" required>
-          Zip / Postal code
+          CEP
         </FormLabel>
         <OutlinedInput
           id="zip"
           name="zip"
-          type="zip"
-          placeholder="12345"
-          autoComplete="shipping postal-code"
+          type="text"
+          placeholder="12345-678"
+          autoComplete="postal-code"
           required
           size="small"
         />
       </FormGrid>
       <FormGrid size={{ xs: 6 }}>
         <FormLabel htmlFor="country" required>
-          Country
+          País
         </FormLabel>
         <OutlinedInput
           id="country"
           name="country"
-          type="country"
-          placeholder="United States"
-          autoComplete="shipping country"
+          type="text"
+          placeholder="Brasil"
+          autoComplete="country"
           required
           size="small"
         />
@@ -127,7 +128,7 @@ export default function AddressForm() {
       <FormGrid size={{ xs: 12 }}>
         <FormControlLabel
           control={<Checkbox name="saveAddress" value="yes" />}
-          label="Use this address for payment details"
+          label="Usar este endereço para detalhes de pagamento"
         />
       </FormGrid>
     </Grid>
